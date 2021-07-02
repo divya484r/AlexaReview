498b4a8956795ffb5e4ceae93b2ce35e5b4f19ed
divya <divya484r@gmail.com>
2021-07-02 20:30:17 +0530

    Initital Commit

---
+++ src/main/java/com/amazon/AlexaReview/AlexaReviewApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.AlexaReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexaReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexaReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amazon/AlexaReview/AlexaReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.AlexaReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexaReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
